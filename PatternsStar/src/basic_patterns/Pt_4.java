a96014a70bee091ec72cb55df5a11fe89326e320
gmanohar21 <manuuuu0321@gmail.com>
2020-06-30 20:30:59 +0530

    First commit

--- PatternsStar/src/basic_patterns/Pt_4.java
+++ PatternsStar/src/basic_patterns/Pt_4.java
@@ -2,10 +2,17 @@ package basic_patterns;
 
 public class Pt_4 {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 5; i--) {
-			for (int j = 1; j <= 5; j++) {
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 6; j >= i; j--) {
+				System.out.print(" ");
+			}
+			for (int j2 = 1; j2 <= i; j2++) {
 				System.out.print("*");
 			}
+			for (int j3 = 2; j3 <= i; j3++) {
+				System.out.print("*");
+			}
+
 			System.out.println();
 		}
 
